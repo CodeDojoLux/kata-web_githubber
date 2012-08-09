5317d317179b01ace1ff5ea8431d4d47eae5e8e3
CodeDojoLux VM-Ubuntu <codedojo.lux@gmail.com>
2012-08-09 07:33:21 +0300

    fixed test

--- test/integration/DefaultGitRestAPIWrapperTest.java
+++ test/integration/DefaultGitRestAPIWrapperTest.java
@@ -6,12 +6,18 @@ import services.DefaultGitRestAPIWrapper;
 
 import static org.fest.assertions.Assertions.assertThat;
 
+import static play.test.Helpers.*;
+
 public class DefaultGitRestAPIWrapperTest {
     @Test
     public void getJsonAsText() {
-        DefaultGitRestAPIWrapper wrapper = new DefaultGitRestAPIWrapper();
-        JsonNode json = wrapper.getGithubWatchersJson("KonstantinG", "todolist-kata");
-        assertThat(json.get(0).get("login").toString()).contains("\"KonstantinG\"");
-        assertThat(json.get(1).get("login").toString()).contains("\"semenodm\"");
+        running(fakeApplication(), new Runnable(){
+            public void run(){
+                DefaultGitRestAPIWrapper wrapper = new DefaultGitRestAPIWrapper();
+                JsonNode json = wrapper.getGithubWatchersJson("KonstantinG", "todolist-kata");
+                assertThat(json.get(0).get("login").toString()).contains("\"KonstantinG\"");
+                assertThat(json.get(1).get("login").toString()).contains("\"semenodm\"");
+            }
+        });
     }
 }
--- test/integration/GitHubberServiceIntegrationTest.java
+++ test/integration/GitHubberServiceIntegrationTest.java
@@ -4,19 +4,29 @@ import org.junit.Test;
 import services.GitHubberService;
 
 import static org.fest.assertions.Assertions.assertThat;
+import static play.test.Helpers.fakeApplication;
+import static play.test.Helpers.running;
 
 public class GitHubberServiceIntegrationTest {
 
 
     @Test public void assert_that_konstantings_todolist_kata_has_semenodm_follower(){
-        GitHubberService githubberService = new GitHubberService();
-        assertThat(githubberService.enquery("konstanting", "todolist-kata").size()).isEqualTo(2);
-        assertThat(githubberService.enquery("konstanting", "todolist-kata")).contains("semenodm");
+        running(fakeApplication(), new Runnable(){
+            public void run(){
+                GitHubberService githubberService = new GitHubberService();
+                assertThat(githubberService.enquery("konstanting", "todolist-kata").size()).isEqualTo(2);
+                assertThat(githubberService.enquery("konstanting", "todolist-kata")).contains("semenodm");
+            }
+        });
     }
 
     @Test public void assert_that_user_olegz_with_project_s12gxDOT2011_has_18_followers_and_contains_wiwoo_bivas_smougenot_followers(){
-        GitHubberService githubberService = new GitHubberService();
-        assertThat(githubberService.enquery("olegz", "s12gx.2011").size()).isEqualTo(18);
-        assertThat(githubberService.enquery("olegz", "s12gx.2011")).contains("wiwoo").contains("bivas").contains("smougenot");
+        running(fakeApplication(), new Runnable(){
+            public void run(){
+                GitHubberService githubberService = new GitHubberService();
+                assertThat(githubberService.enquery("olegz", "s12gx.2011").size()).isEqualTo(18);
+                assertThat(githubberService.enquery("olegz", "s12gx.2011")).contains("wiwoo").contains("bivas").contains("smougenot");
+            }
+        });
     }
 }
